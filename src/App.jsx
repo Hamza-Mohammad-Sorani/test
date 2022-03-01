d868e56bfb78ad014eefd89ac5f663da5ad8581e
hamza-mohammad-sorani <hamzasorani83@gmail.com>
2022-03-01 23:34:59 +0200

    add all files

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+var titles = [];
+
+function App() {
+  const [ bookTitles,setbookTitles ] = useState( titles );
+  useEffect( () => {
+    callApi();
+    setbookTitles( titles );
+    console.log('from forTest branch')
+    getTitles();
+  },[] );
+  const callApi = () => {
+    axios.get( "https://jsonplaceholder.typicode.com/posts/" ).then(
+      ( response ) => {
+        response.data.forEach(element => {
+          titles.push( element.title );
+        });
+      }
+    ).catch((error) => console.log(error))
+  }
+  const getTitles = () => {
+  console.log(bookTitles)
+  }
+  return (
+    <div className="App">
+      test
+      { bookTitles }
+      <div>
+        { true ? titles.map( ( value,index ) => <div key={ index }>{ value }</div> ): <></>}
+      </div>
+    </div>
+  )
+}
+
+export default App
