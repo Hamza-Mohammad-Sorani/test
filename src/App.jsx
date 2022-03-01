04febcb64e707da9d831c552245711d73b3b1b4c
hamza-mohammad-sorani <hamzasorani83@gmail.com>
2022-03-01 23:39:36 +0200

    i dnot know

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   useEffect( () => {
     callApi();
     setbookTitles( titles );
+    console.log('from forTest branch')
     console.log('from forTest branch2')
     getTitles();
   },[] );
